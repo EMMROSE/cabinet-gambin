c737539c136461a91839e0f1ed2419d68b746be5
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-04-29 13:39:52 +0200

    Articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash.notice = "Thank you! you add successfully a new article."
+      redirect_to root_path
+    else
+      #flash.now[:error] = t('flash.work.error_html')
+      render :new
+    end
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # no need for app/views/books/destroy.html.erb
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :resume, :description, :cover)
+  end
+
+end
