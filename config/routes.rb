021c8478004053ce7c4ca886d313c76f8ff745b9
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-05-19 17:53:24 +0200

    front

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'profil', to: 'pages#profil'
   get 'domaine', to: 'pages#domaine'
+  get 'cabinet', to: 'pages#cabinet'
 
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
