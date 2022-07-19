3eeaf93d121886ec5b686522c3a5e7df5a1f9f00
Emmanuel ROSE <roseemmanuel@hotmail.com>
2022-07-19 22:56:06 +0200

    change title add a new page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'cabinet', to: 'pages#cabinet'
   get 'admin', to: 'pages#admin'
   get 'mention', to: 'pages#mention'
+  get 'new_page', to: 'pages#avocat_droit_de_la_famille_à_cherbourg'
 
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
