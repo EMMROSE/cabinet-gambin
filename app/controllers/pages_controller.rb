64339d51fd3597615c021e956ce589f007909971
Emmanuel ROSE <roseemmanuel@hotmail.com>
2022-12-10 16:13:55 +0100

    DB no longer available for free on heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
 
   # def home
   #   @articles = Article.order(created_at: :desc).first(4)
